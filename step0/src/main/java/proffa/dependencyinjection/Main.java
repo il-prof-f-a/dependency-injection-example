db49e2034b1caab398efc129af56374ad649d3e7
ingfrancescoadriani <ing.francesco.adriani@gmail.com>
2025-12-06 09:22:08 +0100

    pubblicazione senza versione finale

---
+++ step0/src/main/java/proffa/dependencyinjection/Main.java
@@ -0,0 +1,39 @@
+package proffa.dependencyinjection;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Uso: java MainSpaghetti <a> <op> <b>");
+            return;
+        }
+
+        String op = args[1];
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[2]);
+
+        double result;
+
+        if (op.equals("+")) {
+            result = a + b;
+        } else if (op.equals("-")) {
+            result = a - b;
+        } else if (op.equals("*")) {
+            result = a * b;
+        } else if (op.equals("/")) {
+            if (b == 0) {
+                System.out.println("Errore: divisione per zero!");
+                return;
+            }
+            result = a / b;
+        } else if (op.equals("%")) {
+            result = a % b;
+        } else if (op.equals("^")) {
+            result = Math.pow(a, b);
+        } else {
+            System.out.println("Operazione non supportata");
+            return;
+        }
+
+        System.out.println("Risultato: " + result);
+    }
+}
---
+++ step1/src/main/java/proffa/dependency/injection/Main.java
@@ -0,0 +1,7 @@
+package proffa.dependency.injection;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
